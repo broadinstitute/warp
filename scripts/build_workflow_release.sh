e0723f9b68cf42d04f936698333fc48a719e0dec
jessicaway <jway@broadinstitute.org>
2020-07-30 14:08:19 -0400

    specify the skylab tests directory

--- scripts/build_workflow_release.sh
+++ scripts/build_workflow_release.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 declare -r SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 declare -r DSDE_PIPELINES_ROOT=$(cd $(dirname ${SCRIPT_DIR}) && pwd)
 
-declare -r -a ZIP_DIRS=(pipelines structs tasks verification tests)
+declare -r -a ZIP_DIRS=(pipelines structs tasks verification tests/skylab)
 declare -r ZIP_PREFIX=workflow_dependencies
 
 
