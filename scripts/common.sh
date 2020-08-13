19041eddf7715953eb1036158b76c0199db7de98
Jessica Way <jway@broadinstitute.org>
2020-08-13 11:19:51 -0400

    Copy automation scripts from dsde-pipelines (#34)
    * specify the skylab tests directory
    
    * copy scripts from dsde-pipelines

---
+++ scripts/common.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+declare -r DSDE_PIPELINES_ROOT=$(git rev-parse --show-toplevel)
+declare -r -a ENVIRONMENTS=(dev rc prod)
+declare -r ENV_DEV=${ENVIRONMENTS[0]}
+declare -r ENV_RC=${ENVIRONMENTS[1]}
+declare -r ENV_PROD=${ENVIRONMENTS[2]}
+
+function stderr() {
+  >&2 echo "$@"
+}
+
+function get_version_from_changelog() {
+  local -r changelog=${1}
+  echo $(grep -m1 '^#' ${changelog} | cut -f 2 -d ' ')
+}
+
+function get_dependencies_for_wdl() {
+  local -r wdl=${1}
+
+  local -a wdlImports=($(grep '^import ".*$' ${wdl} | cut -d ' ' -f 2 | sed 's|\.\.\/||g' | xargs -n1))
+  local -a subWorkflowImports=()
+  for import in ${wdlImports[@]}; do
+    subWorkflowImports=("${subWorkflowImports[@]}" $(get_dependencies_for_wdl ${import}))
+  done
+  echo ${wdlImports[@]} ${subWorkflowImports[@]}
+}
+
+function get_pipeline_dependencies() {
+  local -r pipeline=${1}
+  local -a basenameDependencies=()
+  local -r dependencies=($(get_dependencies_for_wdl ${pipeline}))
+  for dependency in ${dependencies[@]}; do
+    basenameDependencies+=($(basename ${dependency}))
+  done
+
+  echo ${basenameDependencies[@]} | tr ' ' '\n' | sort -u | tr '\n' ' '
+}
+
+function get_versioned_pipelines() {
+  local -r -a pipelines=($(find ${DSDE_PIPELINES_ROOT}/pipelines -type f -name '*.wdl'))
+  echo ${pipelines[@]}
+}
+
+function get_modified_pipelines() {
+  local -r commitToCompare=${1}
+  local -r -a pipelines=($(get_versioned_pipelines))
+  local -r -a changedWdls=($(git diff --name-only HEAD ${commitToCompare} | grep -E '.*\.wdl' | xargs -n1 basename))
+  local -a modifiedPipelines=()
+  for pipeline in ${pipelines[@]}; do
+    if [[ " ${changedWdls[@]}" =~ " $(basename ${pipeline})" ]]; then
+      modifiedPipelines+=(${pipeline})
+    else
+      dependencies=($(get_pipeline_dependencies ${pipeline}))
+      for changedWdl in ${changedWdls[@]}; do
+        if [[ " ${dependencies[@]}" =~ " ${changedWdl}" ]]; then
+          modifiedPipelines+=(${pipeline})
+          break
+        fi
+      done
+    fi
+  done
+  echo ${modifiedPipelines[@]}
+}
+
+function get_pipelines_to_test() {
+  local -r commitToCompare=${1}
+  local -r -a pipelines=($(get_versioned_pipelines))
+  local -r -a changedFiles=($(git diff --name-only HEAD ${commitToCompare}))
+  local -a pipelinesToTest=()
+
+  if [[ "${changedFiles[@]}" == *"verification/"*  || \
+        "${changedFiles[@]}" == *"genomes_in_the_cloud/workflow_tests/src/main/scala/org/broadinstitute/dsp/pipelines/"* ]]; then
+    pipelinesToTest+=(${pipelines[@]})
+  else
+    local -a modifiedPipelines=($(get_modified_pipelines ${commitToCompare}))
+    for pipeline in ${pipelines[@]}; do
+      # Name comparison includes a leading space to avoid discovering pipelines named with substrings of other pipeline names, i.e. "Arrays" and  "MutilSampleArrays"
+      if [[ " ${modifiedPipelines[@]}" =~ " ${pipeline}" ]]; then
+        pipelinesToTest+=(${pipeline})
+      else
+        pipelineDirectory=$(dirname ${pipeline})
+        pipelineHomeDir=${pipelineDirectory/${DSDE_PIPELINES_ROOT}\//}
+        for changedFile in ${changedFiles[@]}; do
+          if [[ "$(dirname ${changedFile})" == "${pipelineHomeDir}"* ]]; then
+            pipelinesToTest+=(${pipeline})
+            break
+          fi
+        done
+      fi
+    done
+  fi
+
+  echo ${pipelinesToTest[@]}
+}
---
+++ scripts/get_changed_pipeline_worklow_test_args.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+declare SCRIPT_DIR
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+source ${SCRIPT_DIR}/common.sh
+
+declare ALL_PIPELINES=($(get_versioned_pipelines))
+
+function pipeline_to_args() {
+  local -r pipeline=${1}
+  local -r env=${2}
+  local -r test=${3}
+  local -r truth=${4}
+  local -r uncached=${5}
+
+  local -r common_args="--env ${env} -t ${test} -b ${truth} ${uncached}"
+
+  case ${pipeline} in
+    AnnotationFiltration)
+      echo AnnotationFiltration -t ${test};;
+    Arrays)
+      echo Arrays -a Single ${common_args};;
+    MultiSampleArrays)
+      echo Arrays -a Multi ${common_args};;
+    ExomeGermlineSingleSample)
+      echo GermlineSingleSample -d Exome ${common_args};;
+    ExomeReprocessing)
+      echo Reprocessing -d Exome ${common_args};;
+    JointGenotyping)
+      echo JointGenotyping -d Exome ${common_args};
+      echo JointGenotyping -d WGS --env ${env} -t Plumbing -b develop ${uncached};;
+    IlluminaGenotypingArray)
+      echo IlluminaGenotypingArray ${common_args};;
+    ExternalExomeReprocessing)
+      echo ExternalReprocessing -d Exome --env ${env} -t Plumbing -b develop ${uncached};;
+    ExternalWholeGenomeReprocessing)
+      echo ExternalReprocessing -d WGS --env ${env} -t Plumbing -b develop ${uncached};;
+    WholeGenomeGermlineSingleSample)
+      echo GermlineSingleSample -d WGS ${common_args};;
+    WholeGenomeReprocessing)
+      echo Reprocessing -d WGS ${common_args};;
+    ValidateChip)
+      echo ValidateChip ${common_args};;
+    ReblockGVCF)
+      echo ReblockGvcf -d Exome ${common_args};
+      echo ReblockGvcf -d WGS ${common_args};;
+    TargetedSomaticSingleSample)
+      echo SomaticSingleSample -d Targeted ${common_args};;
+    JointGenotypingByChromosomePartOne)
+      continue;;
+    JointGenotypingByChromosomePartTwo)
+      continue;;
+  esac
+}
+
+function main() {
+  local -r gittish=${1}
+  local -r test_all=${2}
+  local -r env=${3}
+  local -r test=${4}
+  local -r truth=${5}
+  local -r uncached=${6}
+
+  local -a changed_pipeline_paths=()
+  local -a args=()
+
+  if ${test_all}; then
+    changed_pipeline_paths=(${ALL_PIPELINES[@]})
+  else
+    changed_pipeline_paths=("$(get_pipelines_to_test ${gittish})")
+  fi
+
+  for changed_pipeline_path in ${changed_pipeline_paths[*]}; do
+    pipeline=$(basename ${changed_pipeline_path} .wdl)
+    arg="$(pipeline_to_args ${pipeline} ${env} ${test} ${truth} ${uncached})"
+    if [[ -n ${arg} ]]; then
+      args+=("${arg}")
+    fi
+  done
+
+  for arg in "${args[@]}"; do
+    echo "${arg}"
+  done
+}
+
+main ${1} ${2} ${3} ${4} ${5} ${6}
