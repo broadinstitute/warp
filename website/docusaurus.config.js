b41015aa5c188e49173b3f58faaf37d205179740
Rex Wang <rexwangcc@gmail.com>
2020-09-24 21:44:36 -0400

    Initialize the doc site.

---
+++ website/docusaurus.config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  title: 'WARP',
+  tagline: 'WDL Analysis Research Pipelines',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Broad Institute',
+  projectName: 'warp',
+  plugins: [],
+  themeConfig: {
+    defaultMode: 'light',
+    navbar: {
+      title: 'WARP',
+      logo: {
+        alt: 'Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'right',
+        },
+        {to: 'blog', label: 'Blog', position: 'right'},
+        {
+          href: 'https://github.com/broadinstitute/warp',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            }
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/broadinstitute/warp',
+            },
+          ],
+        },
+      ],
+      copyright: `BSD-3-Clause Licensed | Copyright © ${new Date().getFullYear()} Data Sciences Platform, Broad Institute. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/broadinstitute/warp/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/broadinstitute/warp/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ website/sidebars.js
@@ -0,0 +1,30 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Docusaurus',
+      items: ['style'],
+    },
+    {
+      type: 'category',
+      label: 'General',
+      items: ['best-practices'],
+    },
+    {
+      type: 'category',
+      label: 'Pipelines',
+      items: [
+        {
+          type: 'category',
+          label: 'Optimus',
+          items: ['optimus/optimus', 'optimus/optimus-methods'],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Features',
+      items: ['mdx'],
+    },
+  ]
+};
