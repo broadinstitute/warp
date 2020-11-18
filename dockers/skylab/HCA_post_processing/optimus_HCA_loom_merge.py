e97690f8d02076713b03c969363c5f56169cf27a
jessicaway <jway@broadinstitute.org>
2020-11-18 13:40:10 -0500

    improvements

--- dockers/skylab/HCA_post_processing/optimus_HCA_loom_merge.py
+++ dockers/skylab/HCA_post_processing/optimus_HCA_loom_merge.py
@@ -49,56 +49,59 @@ def main():
     project_id = args.project_id
     project_name = args.project_name
 
-    attr_dict = {"library_preparation_protocol.library_construction_method": library,
-                 "donor_organism.genus_species": species,
-                 "specimen_from_organism.organ": organ,
-                 "project.provenance.document_id": project_id,
-                 "project.project_core.project_name": project_name
-                 }
-
     expression_data_type_list = []
     optimus_output_schema_version_list = []
     pipeline_versions_list = []
-
+    input_id_metadata_field_list = []
+    input_name_metadata_field_list = []
+    input_id_list = []
+    input_name_list = []
 
     with loompy.new(args.output_loom_file) as dsout:
         for i in range(len(loom_file_list)):
             loom_file = loom_file_list[i]
             with loompy.connect(loom_file) as ds:
                 ds.ca['cell_names'] = ds.ca['cell_names'] + "-" + str(i)
+                # add input_id and input_name as column attributes
+                # num_rows, num_cols = ds.shape
+                # input_id = ds.attrs['input_id']
+                # ds.ca.input_id = [input_id for x in range(num_cols)]
+                # try:
+                    # input_name = ds.attrs['input_name']
+                    # ds.ca.input_name = [input_name for x in range(num_cols)]
+                # except AttributeError:
+                    # pass
 
                 # add global attributes for this file to the running list of global attributes
                 expression_data_type_list.append(ds.attrs["expression_data_type"])
                 optimus_output_schema_version_list.append(ds.attrs["optimus_output_schema_version"])
                 pipeline_versions_list.append(ds.attrs["pipeline_version"])
+                input_id_metadata_field_list.append(ds.attrs["input_id_metadata_field"])
+                input_name_metadata_field_list.append(ds.attrs["input_name_metadata_field"])
+                input_id_list.append(ds.attrs["input_id"])
+                input_name_list.append(ds.attrs["input_name"])
 
                 # filter out cells with low counts n_molecules > 1
-                UMIs =ds.ca['n_molecules']
+                UMIs = ds.ca['n_molecules']
                 cells = np.where(UMIs >= 100)[0]
                 for (ix, selection, view) in ds.scan(items=cells, axis=1, key="gene_names"):
                     dsout.add_columns(view.layers, col_attrs=view.ca, row_attrs=view.ra)
 
-
-        # add input_id and input_name as column attributes
-        #num_rows, num_cols = ds.shape
-        #input_id = ds.attrs['input_id']
-        #ds.ca.input_id = [input_id for x in range(num_cols)]
-        #try:
-            #input_name = ds.attrs['input_name']
-            #ds.ca.input_name = [input_name for x in range(num_cols)]
-        #except AttributeError:
-            #pass
-
-        # add global attributes for this file to the running list of global attributes
+    # add global attributes for this file to the running list of global attributes
     ds = loompy.connect(args.output_loom_file)
 
-    attr_dict["expression_data_type"] = ", ".join(set(expression_data_type_list))
-    attr_dict["optimus_output_schema_version"] = ", ".join(set(optimus_output_schema_version_list))
-    attr_dict["pipeline_version"] = ", ".join(set(pipeline_versions_list))
-
-    # alter the global attributes of the combired loom file
-    ds.attrs = attr_dict
-    #loompy.create(args.output_loom_file, ds.layers, ds.ra, ds.ca, file_attrs=attr_dict)
+    ds.attrs["library_preparation_protocol.library_construction_method"] = library
+    ds.attrs["donor_organism.genus_species"] = species
+    ds.attrs["specimen_from_organism.organ"] = organ
+    ds.attrs["project.provenance.document_id"] = project_id
+    ds.attrs["project.project_core.project_name"] = project_name
+    ds.attrs["expression_data_type"] = ", ".join(set(expression_data_type_list))
+    ds.attrs["optimus_output_schema_version"] = ", ".join(set(optimus_output_schema_version_list))
+    ds.attrs["pipeline_version"] = ", ".join(set(pipeline_versions_list))
+    ds.attrs["input_id_metadata_field"] = ", ".join(set(input_id_metadata_field_list))
+    ds.attrs["input_name_metadata_field"] = ", ".join(set(input_name_metadata_field_list))
+    ds.attrs["input_id"] = ", ".join(set(input_id_list))
+    ds.attrs["input_name"] = ", ".join(set(input_name_list))
 
     ds.close()
 
