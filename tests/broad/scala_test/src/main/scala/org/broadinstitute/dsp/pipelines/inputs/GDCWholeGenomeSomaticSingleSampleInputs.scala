12a7b461edc7a1983749f10511493a5841c2d369
nikellepetrillo <npetrill@broadinstitute.org>
2021-01-27 10:11:34 -0500

    add imports

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/inputs/GDCWholeGenomeSomaticSingleSampleInputs.scala
+++ tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/inputs/GDCWholeGenomeSomaticSingleSampleInputs.scala
@@ -1,7 +1,10 @@
 package org.broadinstitute.dsp.pipelines.inputs
 
+import cats.syntax.either._
+import io.circe.parser._
 
 class GDCWholeGenomeSomaticSingleSampleInputs(inputs: String) {
+  import org.broadinstitute.clio.JsonUtils.JsonOps
 
   private val parsed = parse(inputs).valueOr { e =>
     throw new RuntimeException("Could not get JSON object from inputs", e)
