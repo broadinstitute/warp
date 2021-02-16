246cca5d910c3bcc300cf9d8a75695594f86d971
Kishori Mohan Konwar <kishori82@gmail.com>
2021-02-16 11:52:26 -0500

    addressed Jessica's comments

--- dockers/cemba/vcftoallc/convert-vcf-to-allc.py
+++ dockers/cemba/vcftoallc/convert-vcf-to-allc.py
@@ -3,58 +3,52 @@ import argparse
 import gzip
 import re
 
-COMMENT = re.compile(r"^#")
-
-
 def main():
+    """ This script converts a VCF to ALLC file format
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-      "--input_vcf", "-i", dest="input_vcf", default=None, help="input VCF"
+      "--input_vcf", "-i", dest="input_vcf", default=None, required=True, help="input VCF"
     )
     parser.add_argument(
-      "--output_allc", "-o", dest="output_allc", default=None,
-      help="output ALLC"
+      "--output_allc", "-o", dest="output_allc", default=None, help="output ALLC"
     )
     args = parser.parse_args()
 
     writeBinary = False
-    if args.input_vcf:
-        with gzip.open(args.input_vcf, "rt") if args.input_vcf.endswith(
-            ".gz"
-        ) else open(args.input_vcf, "r") as vcf_file:
-            with gzip.open(args.output_allc, "wb") if args.output_allc.endswith(
-                ".gz"
-            ) else open(args.output_allc, "w") as allc_file:
+    with gzip.open(args.input_vcf, "rt") if args.input_vcf.endswith(".gz"
+    ) else open(args.input_vcf, "r") as vcf_file:
+        with gzip.open(args.output_allc, "wb") if args.output_allc.endswith(".gz"
+        ) else open(args.output_allc, "w") as allc_file:
 
-                writeBinary = args.output_allc.endswith(".gz")
+            writeBinary = args.output_allc.endswith(".gz")
 
-                for line in vcf_file:
-                    if COMMENT.search(line):
-                        continue
-                    lineSplits = line.strip().split("\t")
+            for line in vcf_file:
+                if line.startswith("#"):
+                    continue
+                lineSplits = line.strip().split("\t")
 
-                    testInfo = lineSplits[7].split(";")
-                    testContext = re.sub("REFERENCE_CONTEXT=", "", testInfo[2])
-                    testUnMethCov = int(re.sub("CONVERTED_BASE_COV=", "", testInfo[0]))
-                    testMethCov = int(re.sub("UNCONVERTED_BASE_COV=", "", testInfo[3]))
-                    testTotCov = testMethCov + testUnMethCov
+                testInfo = lineSplits[7].split(";")
+                testContext = re.sub("REFERENCE_CONTEXT=", "", testInfo[2])
+                testUnMethCov = int(re.sub("CONVERTED_BASE_COV=", "", testInfo[0]))
+                testMethCov = int(re.sub("UNCONVERTED_BASE_COV=", "", testInfo[3]))
+                testTotCov = testMethCov + testUnMethCov
 
-                    ALLC = [lineSplits[0], lineSplits[1]]
+                ALLC = [lineSplits[0], lineSplits[1]]
 
-                    if lineSplits[3] == "G":
-                        ALLC.append("-")
-                    else:
-                        ALLC.append("+")
-                    ALLC.append(testContext)
-                    ALLC.append(str(testMethCov))
-                    ALLC.append(str(testTotCov))
-                    ALLC.append("1")
+                if lineSplits[3] == "G":
+                    ALLC.append("-")
+                else:
+                    ALLC.append("+")
+                ALLC.append(testContext)
+                ALLC.append(str(testMethCov))
+                ALLC.append(str(testTotCov))
+                ALLC.append("1")
 
-                    if writeBinary:
-                        allc_file.write(("\t".join(ALLC) + "\n").encode())
-                    else:
-                        allc_file.write("\t".join(ALLC) + "\n")
-
+                if writeBinary:
+                    allc_file.write(("\t".join(ALLC) + "\n").encode())
+                else:
+                    allc_file.write("\t".join(ALLC) + "\n")
 
 if __name__ == "__main__":
     main()
