2ff9c674a87c57cb79afbaa628bd0c24272b124a
Wes Dingman <wdingman@broadinstitute.org>
2021-07-14 11:35:18 -0400

    Add skeleton for reset command

---
+++ wreleaser/cmd/reset.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// resetCmd represents the reset command
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Clear the directory which caches WARP releases",
+	Long: `'reset' command clears the directory of cached WARP releases
+
+Usage example:
+
+        'wreleaser reset' (clear the default cache located at $HOME/.wreleaser/cache.json)
+
+        'wreleaser reset --cachedir=/path/to/custom' (clear the custom cache located at /path/to/custom)`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("reset called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(resetCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// resetCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// resetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
