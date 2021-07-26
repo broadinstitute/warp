a96d95b11b0f4cab4b0141390c4face0f4ed7841
ekiernan <55763654+ekiernan@users.noreply.github.com>
2021-07-26 12:36:22 -0400

    Lk graphics updates (#369)
    * Updated WARP docsite graphics

--- website/src/components/HomepageFeatures.js
+++ website/src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Supporting your “omics” research',
-    Svg: require('../../static/img/red.svg').default,
+    Svg: require('../../static/img/DNA200.svg').default,
     description: (
       <>
         WARP pipelines process a range of genomic, genotyping, transcriptomic and epigenomic data.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Delivering production-level quality',
-    Svg: require('../../static/img/yellow.svg').default,
+    Svg: require('../../static/img/Gears200.svg').default,
     description: (
       <>
         Pipelines are used for production by the Broad Genomics Platform and the Human Cell Atlas; they are scientifically validated, scalable, and cost- and cloud- optimized
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Empowering reproducible science',
-    Svg: require('../../static/img/blue.svg').default,
+    Svg: require('../../static/img/Reproducible200.svg').default,
     description: (
       <>
         Pipelines are versioned and open-source, allowing you to track and share exactly how your data was processed.
