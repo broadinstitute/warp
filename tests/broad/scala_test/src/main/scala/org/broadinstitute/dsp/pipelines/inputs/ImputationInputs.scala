26969cc58a3e1a59e1d377f1cf92d61c95ad1e03
cgold <cgold@broadinstitute.org>
2021-10-04 16:33:25 -0400

    attempting to handle optional inputs

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/inputs/ImputationInputs.scala
+++ tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/inputs/ImputationInputs.scala
@@ -22,15 +22,16 @@ class ImputationInputs(inputs: String) {
   def getSplitOutputToSingleSample(workflowName: String): Boolean =
     parsed.unsafeGet[Boolean](s"$workflowName.split_output_to_single_sample")
 
-  def getSingleSampleVcfs(workflowName: String): Seq[String] =
-    parsed.unsafeGet[Seq[String]](s"$workflowName.single_sample_vcfs")
+  def getSingleSampleVcfs(workflowName: String): Option[Seq[String]] =
+    parsed.unsafeGet[Option[Seq[String]]](s"$workflowName.single_sample_vcfs")
 
-  def getSingleSampleVcfsIndices(workflowName: String): Seq[String] =
-    parsed.unsafeGet[Seq[String]](s"$workflowName.single_sample_vcf_indices")
+  def getSingleSampleVcfsIndices(workflowName: String): Option[Seq[String]] =
+    parsed.unsafeGet[Option[Seq[String]]](
+      s"$workflowName.single_sample_vcf_indices")
 
-  def getMultiSampleVcf(workflowName: String): String =
-    parsed.unsafeGet[String](s"$workflowName.multi_sample_vcf")
+  def getMultiSampleVcf(workflowName: String): Option[String] =
+    parsed.unsafeGet[Option[String]](s"$workflowName.multi_sample_vcf")
 
-  def getMultiSampleVcfIndices(workflowName: String): String =
-    parsed.unsafeGet[String](s"$workflowName.multi_sample_vcf_index")
+  def getMultiSampleVcfIndices(workflowName: String): Option[String] =
+    parsed.unsafeGet[Option[String]](s"$workflowName.multi_sample_vcf_index")
 }
