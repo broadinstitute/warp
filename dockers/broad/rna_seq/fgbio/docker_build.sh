816d68626586ecbdd4b8926bc3c25239598ae162
Takuto Sato <tsato@broadinstitute.org>
2021-12-20 18:23:50 -0500

    TCAP RNA-seq Pipeline (#474)
    * initial version of TCA RNA-seq pipeline, copied from ts_ck_rna_stable branch in palantir-workflows
    
    * Copied tag's star docker to a publicly accessible location.
    * Added all CollectMultipleMetrics outputs to pipeline outputs.
    * Prefix all metric outputs with the source (rnaseqc2 / picard)
    * Added unified metrics output (#536)
    * use static fingerprinting file in accessible location
    * GL-1867 - Add RNAWithUmis pipeline to warp test framework. (#531)
    * Added pipeline_version, initial version of a changelog.
    * GL-1808 Allow RNAWithUMIs to take fastqs as input (#533)
    * GL-1858 TCAP Docker
    ** Add fgbio container
    ** Adding umi-tools docker files
    ** Use default picard jar (2.26.8) where possible
    * Updated plumbing and scientific data from Takuto! (both bams and fastqs)

---
+++ dockers/broad/rna_seq/fgbio/docker_build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -e
+
+# Update verson when changes to Dockerfile are made
+DOCKER_IMAGE_VERSION=1.0.0
+TIMESTAMP=$(date +"%s")
+DIR=$(cd $(dirname $0) && pwd)
+
+# Registries and tags
+GCR_URL="us.gcr.io/broad-gotc-prod/fgbio"
+QUAY_URL="quay.io/broadinstitute/gotc-prod-fgbio"
+
+# Fgbio version
+FGBIO_VERSION="1.4.0"
+
+# Necessary tools and help text
+TOOLS=(docker gcloud)
+HELP="$(basename "$0") [-h|--help] [-t|--tools] -- script to build the fgbio image and push to GCR & Quay
+
+where:
+    -h|--help Show help text
+    -v|--version of fgbio to use (default: $FGBIO_VERSION)
+    -t|--tools Show tools needed to run script
+    "
+
+function main(){
+    for t in "${TOOLS[@]}"; do which $t >/dev/null || ok=no; done
+        if [[ $ok == no ]]; then
+            echo "Missing one of the following tools: "
+            for t in "${TOOLS[@]}"; do echo "$t"; done
+            exit 1
+        fi
+
+    while [[ $# -gt 0 ]]
+    do 
+    key="$1"
+    case $key in
+        -v|--version)
+        FGBIO_VERSION="$2"
+        shift
+        shift
+        ;;
+        -h|--help)
+        echo "$HELP"
+        exit 0
+        ;;
+        -t|--tools)
+        for t in "${TOOLS[@]}"; do echo $t; done
+        exit 0
+        ;;
+        *)
+        shift
+        ;;
+    esac
+    done
+
+    IMAGE_TAG="$DOCKER_IMAGE_VERSION-$FGBIO_VERSION-$TIMESTAMP"
+
+    echo "building and pushing GCR Image - $GCR_URL:$IMAGE_TAG"
+    docker build --no-cache -t "$GCR_URL:$IMAGE_TAG" \
+        --build-arg FGBIO_VERSION="$FGBIO_VERSION" "$DIR"
+    docker push "$GCR_URL:$IMAGE_TAG"
+
+    #echo "tagging and pushing Quay Image"
+    #docker tag "$GCR_URL:$IMAGE_TAG" "$QUAY_URL:$IMAGE_TAG"
+    #docker push "$QUAY_URL:$IMAGE_TAG"
+
+    echo "$GCR_URL:$IMAGE_TAG" >> "$DIR/docker_versions.tsv"
+    echo "done"
+}
+
+main "$@"
