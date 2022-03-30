eee7dad3ccb518dfb2bd3fcc867bca7dd57ee67a
Wes Dingman <wdingman@broadinstitute.org>
2022-03-30 09:47:50 -0400

    Remove cromwellAuth url from Cromwell environment

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/CromwellEnvironment.scala
+++ tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/CromwellEnvironment.scala
@@ -10,7 +10,6 @@ import scala.collection.immutable
 
 sealed trait CromwellEnvironment extends EnumEntry {
   def cromwellUrl: URL
-  def cromwellUrlAuth: URL
   def environmentOptions: Seq[(String, Json)] = Seq.empty
   def picardEnv: String
 }
@@ -24,25 +23,18 @@ object CromwellEnvironment extends Enum[CromwellEnvironment] {
   case object Dev extends CromwellEnvironment {
     override val cromwellUrl = new URL(
       "https://cromwell.gotc-dev.broadinstitute.org")
-    override val cromwellUrlAuth = new URL(
-      "https://cromwell-gotc-auth.gotc-dev.broadinstitute.org")
     override val picardEnv: String = "dev"
   }
 
   case object Staging extends CromwellEnvironment {
     override val cromwellUrl = new URL(
       "https://cromwell.gotc-staging.broadinstitute.org")
-    override val cromwellUrlAuth = new URL(
-      "https://cromwell-gotc-auth.gotc-staging.broadinstitute.org")
     override val picardEnv: String = "staging"
-
   }
 
   case object Prod extends CromwellEnvironment {
     override val cromwellUrl = new URL(
       "https://cromwell.gotc-prod.broadinstitute.org")
-    override val cromwellUrlAuth = new URL(
-      "https://cromwell-gotc-auth.gotc-prod.broadinstitute.org")
     override val picardEnv: String = "prod"
   }
 
@@ -50,8 +42,6 @@ object CromwellEnvironment extends Enum[CromwellEnvironment] {
     override val cromwellUrl = new URL(
       "https://cromwell-test.gotc-dev.broadinstitute.org"
     )
-    override val cromwellUrlAuth = new URL(
-      "https://cromwell-gotc-auth.gotc-dev.broadinstitute.org")
     override val picardEnv: String = "dev"
     override val environmentOptions: Seq[(String, Json)] = Seq(
       "jes_gcs_root" -> "gs://broad-gotc-dev-execution1".asJson,
@@ -63,19 +53,16 @@ object CromwellEnvironment extends Enum[CromwellEnvironment] {
     override val cromwellUrl = new URL(
       "https://cromwell-pharma5.gotc-prod.broadinstitute.org"
     )
-    override val cromwellUrlAuth = new URL(
-      "https://cromwell-gotc-auth.gotc-prod.broadinstitute.org")
     override val environmentOptions: Seq[(String, Json)] = Seq(
       "jes_gcs_root" -> "gs://broad-pharma5-execution1/".asJson,
       "google_project" -> "broad-pharma5-compute1".asJson,
     )
     override val picardEnv: String = "prod"
   }
+
   case object JGDev extends CromwellEnvironment {
     override val cromwellUrl = new URL(
       "https://cromwell-jg.gotc-dev.broadinstitute.org")
-    override val cromwellUrlAuth = new URL(
-      "https://cromwell-gotc-auth.gotc-prod.broadinstitute.org")
     override val picardEnv: String = "dev"
     override val environmentOptions: Seq[(String, Json)] = Seq(
       "jes_gcs_root" -> "gs://broad-gotc-dev-execution1".asJson,
@@ -86,8 +73,6 @@ object CromwellEnvironment extends Enum[CromwellEnvironment] {
   case object JGProd extends CromwellEnvironment {
     override val cromwellUrl = new URL(
       "https://cromwell-jg.gotc-prod.broadinstitute.org")
-    override val cromwellUrlAuth = new URL(
-      "https://cromwell-gotc-auth.gotc-prod.broadinstitute.org")
     override val picardEnv: String = "prod"
     override val environmentOptions: Seq[(String, Json)] = Seq(
       "jes_gcs_root" -> "gs://broad-gotc-prod-execution1".asJson,
