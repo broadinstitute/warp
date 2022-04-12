ed32401529c7321d28a84179e1674d22ece36cbe
khajoue2 <fkhajoue@broadinstitute.org>
2022-04-12 18:20:30 -0400

    Updated merging script and docker

--- dockers/skylab/create-npz-output/create-merged-npz-output.py
+++ dockers/skylab/create-npz-output/create-merged-npz-output.py
@@ -57,15 +57,29 @@ def main():
                         help="The matrix file")
 
     args = parser.parse_args()
+    if(len(args.barcodes) != len(args.matrix)):
+      print("Number of barcode files are not the same as number of count matrices.")
+      exit(1)
+    elif(len(args.features) != len(args.matrix)):
+      print("Number of feature files are not the same as number of count matrices.")
+      exit(1)
+    elif(len(args.barcodes) != len(args.features)):
+      print("Number of barcode files are not the same as number of feature files.")
+      exit(1)
 
     # features list file 
-    features_list = read_features_list(args.features[0])
+    features_list = []
+    for i in range(len(args.features)):
+      features_list = np.union1d(features_list, read_features_list(args.features[i]))
+      print(len(features_list))
     np.save("sparse_counts_col_index.npy", features_list)
-
     # read the barcodes file and create the barcode to index
-    barcode_list = read_barcodes_list(args.barcodes[0])
+    barcodes_list = []
+    for i in range(len(args.barcodes)):
+      barcodes_list = np.union1d(barcodes_list, read_barcodes_list(args.barcodes[i]))
+      print(len(barcodes_list))
 
-    #TODO make sure the args.matrix, args.barcodes args features are of equal length
+    np.save("sparse_counts_row_index.npy", barcodes_list)
     data_dict = {}
     for i in range(len(args.matrix)):
         # convert the mtx file to the matrix
@@ -76,27 +90,9 @@ def main():
                  data_dict[(row,col)] = 0
              data_dict[(row,col)] += count
         
-    shape = matrix.shape
-    data = []
-    rows = []
-    cols = []
-    for (row, col), count in data_dict.items():
-        rows.append(row)
-        cols.append(col)
-        data.append(count)
-         
-    coo_matrix = scipy.sparse.coo_matrix((data, (rows, cols)), shape=shape)
-
-    # we need to keep only those rows that have non-zero reads/counts
-    matrix = coo_matrix.tocsr()
-    #nonzero_row_indices, _ = matrix.nonzero()
     #unique_nonzero_row_indices = np.sort(np.unique(nonzero_row_indices))
-    #scipy.sparse.save_npz("sparse_counts.npz", matrix[unique_nonzero_row_indices, :], compressed=True)
     scipy.sparse.save_npz("sparse_counts.npz", matrix, compressed=True)
 
-    # we need to keep only those barcodes that have non-zero reads/counts
-    #np.save("sparse_counts_row_index.npy", barcode_list[unique_nonzero_row_indices])
-    np.save("sparse_counts_row_index.npy", barcode_list)
 
 if __name__ == '__main__':
     main()
