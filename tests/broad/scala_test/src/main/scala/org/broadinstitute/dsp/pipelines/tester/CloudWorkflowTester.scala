ac3c47d4ee1ae2f6d3fbf7403b85d87ba9df1573
Wes Dingman <wdingman@broadinstitute.org>
2022-06-16 10:32:02 -0400

    Add CheckFingerprint to scala tests

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/tester/CloudWorkflowTester.scala
+++ tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/tester/CloudWorkflowTester.scala
@@ -159,7 +159,8 @@ class CloudWorkflowTester(testerConfig: CloudWorkflowConfig)(
       workflowName + ".google_account_vault_path" -> googleAccountVaultPath.asJson,
     )
 
-    val tokenPipelines = List("Arrays", "BroadInternalRNAWithUMIs")
+    val tokenPipelines =
+      List("Arrays", "BroadInternalRNAWithUMIs", "CheckFingerprint")
     val externalPipelines =
       List("ExternalWholeGenomeReprocessing", "ExternalExomeReprocessing")
 
