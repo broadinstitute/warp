717c3f765a2803574fb985fbc74d2981d1aed735
Wes Dingman <wdingman@broadinstitute.org>
2022-06-27 12:22:43 -0400

    Wd sc atac scala (#751)
    * SS2 single sample test
    
    * Fix import dependency issues
    
    * Revert validation wdl name
    
    * Update changelogs
    
    * Missing parentheses in PipelineTestType
    
    * Changelogs again
    
    * MultiSampleSS2 scala
    
    * MultiSampleSS2SingleNucleus scala
    
    * wd-scATAC-scala

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/PipelineTestType.scala
+++ tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/PipelineTestType.scala
@@ -158,6 +158,18 @@ object PipelineTestType extends Enum[PipelineTestType] {
         "SmartSeq2SingleSample",
         "/skylab/smartseq2_single_sample"
       )
+  case object MultiSampleSmartSeq2
+      extends PipelineTestType(
+        "TestMultiSampleSmartSeq2",
+        "MultiSampleSmartSeq2",
+        "/skylab/smartseq2_multisample"
+      )
+  case object MultiSampleSmartSeq2SingleNucleus
+      extends PipelineTestType(
+        "TestMultiSampleSmartSeq2SingleNucleus",
+        "MultiSampleSmartSeq2SingleNucleus",
+        "/skylab/smartseq2_single_nucleus_multisample"
+      )
   case object WholeGenomeReprocessing
       extends PipelineTestType(
         "TestWholeGenomeReprocessing",
