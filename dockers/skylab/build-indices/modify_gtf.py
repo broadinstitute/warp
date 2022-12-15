c7176d4079823017741f5257349f3f8965235198
khajoue2 <fkhajoue@broadinstitute.org>
2022-12-15 14:24:17 -0500

    updated modify gtf file to find the reference source file

--- dockers/skylab/build-indices/modify_gtf.py
+++ dockers/skylab/build-indices/modify_gtf.py
@@ -2,6 +2,22 @@ import argparse
 import gzip
 import re
 
+def get_ref_source(input_gtf):
+    reference_source = ""
+    with open(input_gtf, 'r') as fh:
+        for line_num, line in enumerate(fh):
+            # search string
+            if ('gene_type' in line) or ('transcript_type' in line):
+                reference_source = "Gencode"
+                print('Gencode Reference')
+                # don't continue the search if found at least one instance
+                break
+            elif ('gene_biotype' in line) or ('transcript_biotype' in line):
+                reference_source = "Refseq"
+                print('Refseq reference')
+                # don't continue the search if found at least one instance
+                break
+    return reference_source
 def get_biotypes(biotypes_file_path):
     allowable_biotypes= []
     with open(biotypes_file_path,'r',encoding='utf-8-sig') as biotypesFile:
@@ -15,7 +31,7 @@ def get_biotypes(biotypes_file_path):
 def get_features(features):
     features_dic = {}
     for f in features.split(';'):
-        if f:
+        if f and (len(f.split())>1):
             key = f.split()[0]
             value = f.split()[1]
             if key not in features_dic:
@@ -45,27 +61,47 @@ def modify_attr(features_dic):
         else:
             modified_features = modified_features + key + " "+ features_dic[key] + "; "
     return modified_features
-
-def get_gene_ids(input_gtf,biotypes):
+def get_gene_ids_Refseq(input_gtf,biotypes):
     gene_ids =[]
     with open(input_gtf, 'r') as input_file:
         for line in input_file:
             if not line.startswith('#'):
                 fields = [x.strip() for x in line.strip().split('\t')]
-                if fields[2]=='transcript':
+                if fields[2]=='gene':
                     features = re.sub('"', '', line.strip().split('\t')[8].strip())
                     features_dic = get_features(features)
+                    if ('gene_biotype' in features_dic.keys()) and (features_dic['gene_biotype'] in biotypes):
+                        gene=features_dic['gene_id'].split('.',1)[0]
+                        if gene not in gene_ids:
+                            gene_ids.append(gene)
+    input_file.close()
+    return gene_ids
 
-                    if features_dic['gene_biotype'] in biotypes:
-                        if 'tag' in features_dic:
-                            if ('readthrough_transcript' not in features_dic['tag']) and ('PAR' not in features_dic['tag']):
-                                gene=features_dic['gene_id'].split('.',1)[0]
-                                if gene not in gene_ids:
-                                    gene_ids.append(gene)
-                        else:
-                            gene=features_dic['gene_id'].split('.',1)[0]
-                            if gene not in gene_ids:
-                                gene_ids.append(gene)
+def get_gene_ids_Gencode(input_gtf, biotypes):
+    gene_ids = []
+    with open(input_gtf, 'r') as input_file:
+        for line in input_file:
+            if line.startswith('#'):
+                continue
+            fields = [x.strip() for x in line.strip().split('\t')]
+            if fields[2] != 'transcript':
+                continue
+            features = re.sub('"', '', line.strip().split('\t')[8].strip())
+            features_dic = get_features(features)
+
+            if (features_dic['gene_type'] not in biotypes) or (
+                    features_dic['transcript_type'] not in biotypes):
+                continue
+            if 'tag' in features_dic:
+                if ('readthrough_transcript' not in features_dic['tag']) and (
+                        'PAR' not in features_dic['tag']):
+                    gene=features_dic['gene_id'].split('.', 1)[0]
+                    if gene not in gene_ids:
+                        gene_ids.append(gene)
+            else:
+                gene=features_dic['gene_id'].split('.', 1)[0]
+                if gene not in gene_ids:
+                    gene_ids.append(gene)
 
     input_file.close()
     return gene_ids
@@ -103,8 +139,17 @@ def main():
     args = parser.parse_args()
 
     allowable_biotypes = get_biotypes(biotypes_file_path=args.biotypes_file)
-    gene_ids = get_gene_ids(input_gtf=args.input_gtf,biotypes=allowable_biotypes)
+    print("Filtering the gtf for biotypes:", allowable_biotypes)
+    ref_source = get_ref_source(input_gtf=args.input_gtf)
+    if ref_source == "Refseq":
+        gene_ids = get_gene_ids_Refseq(input_gtf=args.input_gtf, biotypes=allowable_biotypes)
+    elif ref_source == "Gencode":
+        gene_ids = get_gene_ids_Gencode(input_gtf=args.input_gtf, biotypes=allowable_biotypes)
+    else:
+        print("The input gtf file cannot be processed with this script. Provided biotype attributes should be from Gencode or Refseq.")
+        exit(0)
 
+    print("Writing the output file based on selected genes with biotype")
     with open(args.input_gtf, 'r') as input_file:
         with open(args.output_gtf, 'w') as output_gtf:
             for line in input_file:
