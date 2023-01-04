d41f823f4e4100e0ea9cd21ae6f72844d9ca341a
jessicaway <jway@broadinstitute.org>
2023-01-04 10:14:02 -0700

    remove more things

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/Config.scala
+++ tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/Config.scala
@@ -4,27 +4,5 @@ import org.broadinstitute.dsp.pipelines.config._
 
 case class Config(
     test: WorkflowTestType = WorkflowTestType.Dummy,
-    broadInternalRNAWithUMIsConfig: BroadInternalRNAWithUMIsConfig =
-      BroadInternalRNAWithUMIsConfig(),
-    broadInternalUltimaGenomicsConfig: BroadInternalUltimaGenomicsConfig =
-      BroadInternalUltimaGenomicsConfig(),
-    checkFingerprintConfig: CheckFingerprintConfig = CheckFingerprintConfig(),
-    cloudWorkflowConfig: CloudWorkflowConfig = CloudWorkflowConfig(),
-    cramToUnmappedBamsConfig: CramToUnmappedBamsConfig =
-      CramToUnmappedBamsConfig(),
-    genotypeConcordanceConfig: GenotypeConcordanceConfig =
-      GenotypeConcordanceConfig(),
-    gdcWholeGenomeSomaticSingleSampleConfig: GDCWholeGenomeSomaticSingleSampleConfig =
-      GDCWholeGenomeSomaticSingleSampleConfig(),
-    germlineCloudConfig: GermlineCloudWorkflowConfig =
-      GermlineCloudWorkflowConfig(),
-    imputationConfig: ImputationConfig = ImputationConfig(),
-    rnaWithUMIsConfig: RNAWithUMIsConfig = RNAWithUMIsConfig(),
-    somaticCloudWorkflowConfig: SomaticCloudWorkflowConfig =
-      SomaticCloudWorkflowConfig(),
-    ultimaGenomicsWholeGenomeGermlineConfig: UltimaGenomicsWholeGenomeGermlineConfig =
-      UltimaGenomicsWholeGenomeGermlineConfig(),
-    ultimaGenomicsJointGenotypingConfig: UltimaGenomicsJointGenotypingConfig =
-      UltimaGenomicsJointGenotypingConfig(),
-    validateChipConfig: ValidateChipConfig = ValidateChipConfig()
+    cloudWorkflowConfig: CloudWorkflowConfig = CloudWorkflowConfig()
 )
