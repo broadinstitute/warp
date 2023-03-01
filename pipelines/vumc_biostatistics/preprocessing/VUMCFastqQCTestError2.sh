4d5356a8910ff6decfae3864c28a6d3cde0c8e01
Quanhu Sheng <shengqh@gmail.com>
2023-02-28 22:14:25 -0600

    add test for VUMCFastqQC

---
+++ pipelines/vumc_biostatistics/preprocessing/VUMCFastqQCTestError2.sh
@@ -0,0 +1,6 @@
+cd /scratch/cqs/shengq2/test
+
+java -Dconfig.file=/home/shengq2/program/cqsperl/config/wdl/cromwell.local.conf -jar /data/cqs/softwares/wdl/cromwell-84.jar \
+  run /home/shengq2/program/warp/pipelines/vumc_biostatistics/preprocessing/VUMCFastqQC.wdl \
+  -i /home/shengq2/program/warp/pipelines/vumc_biostatistics/preprocessing/VUMCFastqQC.inputs.local.error2.json \
+  --options ~/program_other/si2022/cromwell.options.json
