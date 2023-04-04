9083a409d177418b9b31efb6bd6888419769a6f6
JStolze <117887353+JoeyStolze@users.noreply.github.com>
2023-04-04 12:04:25 -0700

    Create VUMCCramQC.test.combined.sh

---
+++ pipelines/vumc_biostatistics/preprocessing/VUMCCramQC.test.combined.sh
@@ -0,0 +1,6 @@
+cd /nobackup/h_cqs/jstolze/terra/CountCram_testing/incorperating_with_orig_QC/
+java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
+  -jar /data/cqs/softwares/wdl/cromwell-84.jar \
+  run /home/stolzelk/warp/pipelines/vumc_biostatistics/preprocessing/VUMCCramQC.wdl \
+  -i /home/stolzelk/warp/pipelines/vumc_biostatistics/preprocessing/VUMCCramQC.inputs.local.mapped.json \
+  --options /data/cqs/softwares/cqsperl/config/wdl/cromwell.options.json
