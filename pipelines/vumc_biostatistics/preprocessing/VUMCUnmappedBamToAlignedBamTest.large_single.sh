5c5f81ed7202ec7a44c8751038a3a891a0e58877
Quanhu Sheng <shengqh@gmail.com>
2023-04-20 11:58:59 -0500

    update test code

--- pipelines/vumc_biostatistics/preprocessing/VUMCUnmappedBamToAlignedBamTest.large_single.sh
+++ pipelines/vumc_biostatistics/preprocessing/VUMCUnmappedBamToAlignedBamTest.large_single.sh
@@ -1,6 +1,7 @@
-cd /scratch/cqs/shengq2/test/wdl
+cd /nobackup/h_cqs/shengq2/test/wdl
 
-java -Dconfig.file=cromwell.local.conf -jar /data/cqs/softwares/wdl/cromwell-84.jar \
+java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
+  -jar /data/cqs/softwares/wdl/cromwell-84.jar \
   run /home/shengq2/program/warp/pipelines/vumc_biostatistics/preprocessing/VUMCUnmappedBamToAlignedBam.wdl \
   -i /home/shengq2/program/warp/pipelines/vumc_biostatistics/preprocessing/VUMCUnmappedBamToAlignedBam.inputs.local.large_single.json \
   --options ~/program_other/si2022/cromwell.options.json
