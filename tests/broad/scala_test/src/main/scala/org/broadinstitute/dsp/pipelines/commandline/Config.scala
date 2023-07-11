7c96b91a26bdcf675c00bc5475ed9a3deadd2c14
jessicaway <jway@broadinstitute.org>
2023-07-11 10:39:14 -0600

    more cleanup

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/Config.scala
+++ tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/Config.scala
@@ -4,5 +4,6 @@ import org.broadinstitute.dsp.pipelines.config._
 
 case class Config(
     test: WorkflowTestType = WorkflowTestType.Dummy,
+    germlineCloudConfig: GermlineCloudWorkflowConfig = GermlineCloudWorkflowConfig(),
     cloudWorkflowConfig: CloudWorkflowConfig = CloudWorkflowConfig()
 )
