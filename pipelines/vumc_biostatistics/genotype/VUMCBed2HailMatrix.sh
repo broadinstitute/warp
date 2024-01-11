29226f824536e25ca4b76f3c33a7de5ca7978fa3
Quanhu Sheng <shengqh@gmail.com>
2024-01-10 23:36:44 -0600

    test large chromosome

--- pipelines/vumc_biostatistics/genotype/VUMCBed2HailMatrix.sh
+++ pipelines/vumc_biostatistics/genotype/VUMCBed2HailMatrix.sh
@@ -4,8 +4,14 @@ fi
 
 cd /nobackup/h_cqs/shengq2/biovu/cromwell
 
+# java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
+#   -jar /data/cqs/softwares/wdl/cromwell-84.jar \
+#   run /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/genotype/VUMCBed2HailMatrix.wdl \
+#   -i /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/genotype/VUMCBed2HailMatrix.inputs.json \
+#   --options /data/cqs/softwares/cqsperl/config/wdl/cromwell.options.json
+
 java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
   -jar /data/cqs/softwares/wdl/cromwell-84.jar \
   run /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/genotype/VUMCBed2HailMatrix.wdl \
-  -i /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/genotype/VUMCBed2HailMatrix.inputs.json \
+  -i /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/genotype/VUMCBed2HailMatrix.inputs_chrY.json \
   --options /data/cqs/softwares/cqsperl/config/wdl/cromwell.options.json
