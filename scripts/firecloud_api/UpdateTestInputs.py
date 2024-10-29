c620aee40a57686f42e448448e71a0033939ce9c
jessicaway <jway@broadinstitute.org>
2024-10-29 10:20:03 -0600

    Update json formatting script

--- scripts/firecloud_api/UpdateTestInputs.py
+++ scripts/firecloud_api/UpdateTestInputs.py
@@ -10,15 +10,28 @@ def update_test_inputs(test_inputs, truth_path, test_path):
     # - test_path: The path to the test data
     # The test inputs JSON will be updated to include these inputs
 
+    # Get the pipeline name from the test inputs JSON
+    pipeline_name = next(iter(test_inputs)).split('.')[0]
+
+    #Append "Test" in front of the pipeline name
+    test_name = f"Test{pipeline_name}"
+
+    # Update all keys in the json file to replace the pipeline name with the test name
+    for key in list(test_inputs.keys()):
+        new_key = key.replace(pipeline_name, test_name)
+        test_inputs[new_key] = test_inputs.pop(key)
+
     # Add the truth_path and test_path to the test inputs JSON
-    test_inputs["truth_path"] = truth_path
-    test_inputs["test_path"] = test_path
+    test_inputs[f"{test_name}.truth_path"] = truth_path
+    test_inputs[f"{test_name}.test_path"] = test_path
 
     # Save the updated test inputs JSON
     with open("updated_test_inputs.json", 'w') as file:
         json.dump(test_inputs, file, indent=4)
 
     print("Test inputs JSON updated with truth_path and test_path")
+
+
 def main():
     description = """This script updates the test inputs JSON to work with the test wrapper WDL, 
     which runs the pipeline and verification"""
@@ -40,8 +53,8 @@ def main():
     )
 
     parser.add_argument(
-        "--json_inputs",
-        dest="json_inputs",
+        "--inputs_json",
+        dest="inputs_json",
         required=True,
         help="The JSON file containing the test inputs, formatted to run the pipeline WDL. "
              "This will be updated to run the wrapper Test WDL",
@@ -59,7 +72,7 @@ def main():
     args = parser.parse_args()
 
     # Load the JSON file containing the test inputs
-    with open(args.json_inputs, 'r') as file:
+    with open(args.inputs_json, 'r') as file:
         test_inputs = json.load(file)
 
     # Update the test inputs to work with the test wrapper WDL
