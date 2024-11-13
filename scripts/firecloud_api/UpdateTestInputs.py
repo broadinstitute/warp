23f7f8371158ca4897d3bcf61f83ebef07fcfef5
jessicaway <jway@broadinstitute.org>
2024-11-13 10:36:19 -0700

    Update json formatting script

--- scripts/firecloud_api/UpdateTestInputs.py
+++ scripts/firecloud_api/UpdateTestInputs.py
@@ -1,19 +1,27 @@
 import argparse
 import json
+import os
 
 
-def update_test_inputs(test_inputs, truth_path, test_path):
+def update_test_inputs(inputs_json, truth_path, test_path, update_truth):
     # Update the test inputs JSON to work with the test wrapper WDL
     # The test wrapper WDL runs the pipeline WDL and verifies the results
     # The test wrapper WDL requires the following inputs:
     # - truth_path: The path to the truth data
     # - test_path: The path to the test data
+    # - update_truth: Boolean indicating whether truth should be updated, default is False
     # The test inputs JSON will be updated to include these inputs
+
+    with open(inputs_json, 'r') as file:
+        test_inputs = json.load(file)
+
+    # get the sample name from the test inputs JSON, this is needed for tests with multiple inputs
+    sample_name = os.path.splitext(os.path.basename(inputs_json))[0]
 
     # Get the pipeline name from the test inputs JSON
     pipeline_name = next(iter(test_inputs)).split('.')[0]
 
-    #Append "Test" in front of the pipeline name
+    # Append "Test" in front of the pipeline name
     test_name = f"Test{pipeline_name}"
 
     # Update all keys in the json file to replace the pipeline name with the test name
@@ -22,11 +30,13 @@ def update_test_inputs(test_inputs, truth_path, test_path):
         test_inputs[new_key] = test_inputs.pop(key)
 
     # Add the truth_path and test_path to the test inputs JSON
-    test_inputs[f"{test_name}.truth_path"] = truth_path
-    test_inputs[f"{test_name}.test_path"] = test_path
+    test_inputs[f"{test_name}.truth_path"] = f"{truth_path}/{sample_name}/"
+    test_inputs[f"{test_name}.test_path"] = f"{test_path}/{sample_name}/"
+    test_inputs[f"{test_name}.update_truth"] = update_truth
 
     # Save the updated test inputs JSON
-    with open("updated_test_inputs.json", 'w') as file:
+    output_name = f"updated_{sample_name}.json"
+    with open(output_name, 'w') as file:
         json.dump(test_inputs, file, indent=4)
 
     print("Test inputs JSON updated with truth_path and test_path")
@@ -71,12 +81,9 @@ def main():
 
     args = parser.parse_args()
 
-    # Load the JSON file containing the test inputs
-    with open(args.inputs_json, 'r') as file:
-        test_inputs = json.load(file)
-
     # Update the test inputs to work with the test wrapper WDL
-    update_test_inputs(test_inputs, args.truth_path, args.test_path)
+    update_test_inputs(args.inputs_json, args.truth_path, args.test_path, args.update_truth)
+
 
 if __name__ == "__main__":
     main()
---
+++ tasks/broad/TerraCopyFilesFromCloudToCloud.wdl
@@ -0,0 +1,44 @@
+version 1.0
+
+## Copyright Broad Institute, 2024
+##
+## This WDL defines tasks used for moving files from place to place on Terra Platform.
+##
+## Runtime parameters are often optimized for Broad's Google Cloud Platform implementation.
+## For program versions, see docker containers.
+##
+## LICENSING :
+## This script is released under the WDL source code license (BSD-3) (see LICENSE in
+## https://github.com/broadinstitute/wdl). Note however that the programs it calls may
+## be subject to different licenses. Users are responsible for checking that they are
+## authorized to run all programs before running this script. Please see the docker
+## page at https://hub.docker.com/r/broadinstitute/genomes-in-the-cloud/ for detailed
+## licensing information pertaining to the included programs.
+
+task TerraCopyFilesFromCloudToCloud {
+  input {
+    Array[String] files_to_copy
+    String destination_cloud_path
+  }
+
+  command {
+    set -euo pipefail
+
+    gcloud config set storage/process_count 16
+    gcloud config set storage/thread_count  2
+
+    gcloud storage cp ~{sep=' ' files_to_copy} ~{destination_cloud_path}
+  }
+
+  output {
+    Boolean done = true
+  }
+
+  runtime {
+    memory: "16 GiB"
+    cpu: "1"
+    disks: "local-disk 32 HDD"
+    docker: "gcr.io/google.com/cloudsdktool/google-cloud-cli:499.0.0-slim"
+    preemptible: 3
+  }
+}
