01b07a0d8cee278fd6f3749ddb9b316a350a7991
npetrill <npetrill@broadinstitute.org>
2024-11-15 12:27:45 -0500

    remove print bc we arent capturing the right thing

--- scripts/firecloud_api/UpdateTestInputs.py
+++ scripts/firecloud_api/UpdateTestInputs.py
@@ -39,7 +39,6 @@ def update_test_inputs(inputs_json, truth_path, results_path, update_truth):
     with open(output_name, 'w') as file:
         json.dump(test_inputs, file, indent=4)
 
-    print(f"Test inputs JSON updated and saved as: {output_name}")
     return output_name
 
 
