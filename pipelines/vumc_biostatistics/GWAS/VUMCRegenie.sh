829958798a744b0f9c89564e5fa4e5221de9c1e2
Quanhu Sheng <shengqh@gmail.com>
2024-11-26 00:49:32 -0600

    change folder

--- pipelines/vumc_biostatistics/GWAS/VUMCRegenie.sh
+++ pipelines/vumc_biostatistics/GWAS/VUMCRegenie.sh
@@ -1,5 +1,5 @@
-mkdir -p /nobackup/h_cqs/shengq2/biovu/demo
-cd /nobackup/h_cqs/shengq2/biovu/demo
+mkdir -p /nobackup/h_cqs/shengq2/biovu/agd35k/pgen
+cd /nobackup/h_cqs/shengq2/biovu/agd35k/pgen
 java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
   -jar /data/cqs/softwares/wdl/cromwell-84.jar \
   run /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/GWAS/VUMCRegenie.wdl \
