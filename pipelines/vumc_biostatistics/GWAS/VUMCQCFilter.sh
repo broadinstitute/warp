f85e4262d8d33e8bfed833ba85ea800f4729ff7c
Quanhu Sheng <shengqh@gmail.com>
2024-11-26 14:40:10 -0600

    using high mac for testing

--- pipelines/vumc_biostatistics/GWAS/VUMCQCFilter.sh
+++ pipelines/vumc_biostatistics/GWAS/VUMCQCFilter.sh
@@ -1,5 +1,7 @@
 mkdir -p /nobackup/h_cqs/shengq2/biovu/agd35k/pgen
 cd /nobackup/h_cqs/shengq2/biovu/agd35k/pgen
+
+#increased mac to 800 to speed up the process
 java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
   -jar /data/cqs/softwares/wdl/cromwell-84.jar \
   run /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/GWAS/VUMCQCFilter.wdl \
