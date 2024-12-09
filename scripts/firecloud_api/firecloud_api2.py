573a4cab14d7b8895a13265e33e5435bccb9619d
npetrill <npetrill@broadinstitute.org>
2024-12-09 15:41:52 -0500

    dont encode

--- scripts/firecloud_api/firecloud_api2.py
+++ scripts/firecloud_api/firecloud_api2.py
@@ -22,8 +22,12 @@ class FirecloudAPI:
 
     def _build_auth_headers(self):
         scopes = ["profile", "email", "openid"]
+        #sa_credentials = service_account.Credentials.from_service_account_info(
+        #    json.loads(base64.b64decode(self.sa_json_b64).decode("utf-8")), scopes=scopes
+        #)
+        #TODO - Fix this, probably needs to be encoded in base64
         sa_credentials = service_account.Credentials.from_service_account_info(
-            json.loads(base64.b64decode(self.sa_json_b64).decode("utf-8")), scopes=scopes
+            json.loads(self.sa_json_b64), scopes=scopes
         )
         delegated_credentials = sa_credentials.with_subject(self.user)
         token = self._get_user_token(delegated_credentials)
