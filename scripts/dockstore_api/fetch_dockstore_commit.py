29540b13d512da61a9e0a13053f06442aa7db212
npetrill <npetrill@broadinstitute.org>
2024-12-12 15:39:32 -0500

    grabbranch name

--- scripts/dockstore_api/fetch_dockstore_commit.py
+++ scripts/dockstore_api/fetch_dockstore_commit.py
@@ -43,7 +43,6 @@ def fetch_commit_id(token, repository, version_name):
     if not commit_id:
         raise ValueError("Commit ID could not be found.")
 
-    logging.info(f"Dockstore Commit ID: {commit_id}")
     return commit_id
 
 if __name__ == "__main__":
@@ -55,6 +54,6 @@ if __name__ == "__main__":
 
     try:
         commit_id = fetch_commit_id(token, repository, version_name)
-        logging.info(commit_id)
+        logging.info(f"Dockstore Commit ID: {commit_id}")
     except Exception as e:
         logging.error(f"Error: {e}")
