8d6e546bb84a9a99b9a259365d5b8092376359bc
npetrill <npetrill@broadinstitute.org>
2024-12-12 15:41:54 -0500

    grabbranch name

--- scripts/dockstore_api/fetch_dockstore_commit.py
+++ scripts/dockstore_api/fetch_dockstore_commit.py
@@ -1,12 +1,5 @@
 import requests
 import sys
-import logging
-
-# Configure logging to display INFO level and above messages
-logging.basicConfig(
-    level=logging.INFO,  # This will show INFO and higher levels (INFO, WARNING, ERROR, CRITICAL)
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
 
 def fetch_commit_id(token, repository, version_name):
     # Fetch the workflow data
@@ -22,7 +15,6 @@ def fetch_commit_id(token, repository, version_name):
 
     # Extract workflow ID and version ID
     workflow_id = data.get("id")
-
     version_id = next(
         (version["id"] for version in data.get("workflowVersions", [])
          if version["name"] == version_name),
@@ -43,17 +35,18 @@ def fetch_commit_id(token, repository, version_name):
     if not commit_id:
         raise ValueError("Commit ID could not be found.")
 
+    print(f"Dockstore Commit ID: {commit_id}")
     return commit_id
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        logging.error("Usage: python fetch_dockstore_commit.py <token> <repository> <version_name>")
+        print("Usage: python fetch_dockstore_commit.py <token> <repository> <version_name>")
         sys.exit(1)
 
     _, token, repository, version_name = sys.argv
 
     try:
         commit_id = fetch_commit_id(token, repository, version_name)
-        logging.info(f"Dockstore Commit ID: {commit_id}")
+        print(commit_id)
     except Exception as e:
-        logging.error(f"Error: {e}")
+        print(f"Error: {e}")
