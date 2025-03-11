c341fc1a64a653ead9399ce8b094d8a5f04b7ad5
Nikelle Petrillo <38223776+nikellepetrillo@users.noreply.github.com>
2025-03-11 13:29:37 -0400

    Remove old tests (#1543)
    * remove old tests
    
    * move readme to verificiation

--- tests/broad/scala_test/src/main/scala/org/broadinstitute/dsp/pipelines/commandline/ClioConfig.scala
+++
@@ -1,8 +0,0 @@
-package org.broadinstitute.dsp.pipelines.commandline
-
-case class ClioConfig(
-    clioUrl: String,
-    clioPort: Int,
-    clioUseHttps: Boolean,
-    serviceAccountJson: Option[String]
-)
